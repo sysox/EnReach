e2a6beb6933fd0c318bcc5b32c0858a45b55540c
sysox <marek.sys@gmail.com>
2025-08-06 16:23:43 +0200

    cleaning

--- text_to_speech/spell.py
+++ text_to_speech/spell.py
@@ -1,20 +1,25 @@
 from gtts import gTTS
-from playsound import playsound
+import pyttsx3
 
-def text_to_speech(text, lang='en', slow=False, filename='data/tmp.mp3'):
-    """
-    Convert text to speech and save it as an mp3 file.
+# from playsound import playsound
 
-    :param text: The text to convert to speech.
-    :param lang: The language for the speech (default is English).
-    :param slow: If True, the speech will be slower (default is False).
-    :param filename: The name of the file to save the speech (default is 'data/tmp.mp3').
-    """
+def text_to_speech(english_text):
+    engine = pyttsx3.init()
+    engine.say(english_text)
+    engine.runAndWait()
+    engine.stop()
+
+def text_to_mp3(text, lang='en', slow=False, filename='data/tmp.mp3'):
     tts = gTTS(text=text, lang=lang, slow=slow)
     tts.save(filename)
-    playsound(filename)
+
+
 
 if __name__ == "__main__":
-    text = "Spotreba granátov je taká vysoká, že nie je čas na dodržiavanie noriem – munície je potom dosť, ale lieta si, kam sa jej zachce."
-    text_to_speech(text, lang='sk', slow=False, filename='data/tmp.mp3')
+    slovak_text = "Spotreba granátov je taká vysoká, že nie je čas na dodržiavanie noriem – munície je potom dosť, ale lieta si, kam sa jej zachce."
+    english_translation = "Grenade consumption is so high that there is no time to comply with standards – there is then enough ammunition, but it flies wherever it wants."
+
+    text_to_mp3(slovak_text, lang='sk', slow=False, filename='data/tmp.mp3')
+    text_to_mp3(english_translation, lang='en', slow=False, filename='data/tmp.mp3')
+    text_to_speech(english_translation)
 
